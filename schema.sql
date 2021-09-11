c076359d9576b80a324f6488263857d930e18062
Claudia Canamas-Donnelly <cdonnelly@efclocal.com>
2021-09-11 12:15:16 -0400

    fixed df col names

--- schema.sql
+++ schema.sql
@@ -5,15 +5,14 @@ PRIMARY KEY (movie_ID)
 );
 
 CREATE TABLE Film_Performance(
-year INTEGER,
+nomination_ID INTEGER,
+Year INTEGER,
 WorldwideBoxOffice FLOAT,
 DomesticBoxOffice FLOAT,
 InternationalBoxOffice FLOAT,
 category VARCHAR(80),
-name VARCHAR(150),
 winner BOOLEAN,
 nomination_count INTEGER,
-nomination_ID INTEGER,
 movie_ID INTEGER,
 PRIMARY KEY(nomination_ID),
 FOREIGN KEY (movie_ID) REFERENCES Movies (movie_ID)
